6396f2ddcffc2e5df64e9b4d34e98812bacd86eb
Marc Hauschildt <marc.hauschildt@gmail.com>
2023-01-19 21:18:44 -0600

    Initial commit

---
+++ src/main/java/com/hauschildt/java3project/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.hauschildt.java3project;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = res.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
